2e10066f5609908352cb5e992a0eb75ff5609fd5
Onur Nesvat <onurnesvat@gmail.com>
2026-03-28 09:55:21 +0300

    v1.1.3

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ import { ApiBackend } from "./lib/api-backend.js";
 import { DropboxBackend } from "./lib/dropbox-backend.js";
 import { checkHealth, DEFAULT_API_URL } from "./lib/api-client.js";
 
-const VERSION = "1.1.0";
+const VERSION = "1.1.3";
 
 let selectedBackend: Backend | null = null;
 
